4290f7797a96447a2551b17abec9c4a462b4cfc0
FERDEVIL87 <ferdevilx@gmail.com>
2025-06-24 22:26:25 +0700

    4

--- api/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Arahkan request ke file index.php publik Laravel
-require __DIR__ . '/../public/index.php';
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique(); // Kolom dari error sebelumnya
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            // PASTIKAN BARIS INI ADA
-            $table->string('role')->default('user'); // Menambahkan kolom 'role' dengan default 'user'
             $table->rememberToken();
             $table->timestamps();
         });
